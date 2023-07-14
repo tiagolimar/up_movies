7731739db6cd4622cac9d0cb16422ca81eb10045
tiagolimar <tiagolima.r2@gmail.com>
2023-07-13 21:52:14 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
+import { useState } from "react";
+
 function App() {
+  const [fields, setFields] = useState({
+    'titulo':'',
+    'sinopse':'',
+    'votos':''
+  })
+
+  function updateFilm(event){
+    setFields({...fields,[event.target.id]:event.target.value})
+    console.log(fields)
+  }
+
+  function campo(titulo,id_name,fn){
+    return(
+      <div className="col-md-4">
+      <label htmlFor="" className="form-label">{titulo}</label>
+      <input 
+        type="text"
+        id={id_name}
+        className="form-control" 
+        onChange={fn} />
+    </div>
+    )
+  }
+
   return (
     <>
       <div className="container pt-4">
         <h2>Cadastro de Filmes</h2>
         <form className="row g-3">
-          <div className="col-md-4">
-            <label htmlFor="" className="form-label">
-              Título
-            </label>
-            <input type="text" className="form-control" />
-          </div>
-          <div className="col-md-4">
-            <label htmlFor="" className="form-label">
-              Sinopse
-            </label>
-            <input type="text" className="form-control" />
-          </div>
-          <div className="col-md-4">
-            <label htmlFor="" className="form-label">
-              Votos
-            </label>
-            <input type="text" className="form-control" />
-          </div>
+          {campo('Título','titulo',updateFilm)}
+          {campo('Sinopse','sinopse',updateFilm)}
+          {campo('Votos','votos',updateFilm)}
           <button className="btn btn-dark">Adicionar</button>
         </form>
       </div>
